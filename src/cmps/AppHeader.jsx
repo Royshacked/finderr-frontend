06202c0b8620bd4d889cc75c3bddefbd68d1425d
yeshayahu shapira <shapira213@gmail.com>
2024-08-27 17:30:27 +0300

    ALL CHANGES

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -5,6 +5,8 @@ import Hire from '../assets/svg/homepage/hire.svg?react'
 import FreeLancer from '../assets/svg/homepage/freelancer.svg?react'
 import { GigCategoriesBar } from "./GigCategoriesBar.jsx"
 import { SimpleSlider } from '../cmps/Carusela copy.jsx'
+import { useState } from "react"
+
 export function AppHeader() {
 
 
@@ -116,12 +118,7 @@ export function AppHeader() {
 
 				</nav>
 			</header >
+		</div>
 
-			)
+	)
 }
-
-			{/* <svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19"><rect y="16" width="23" height="3" rx="1.5" fill="#555"></rect><rect width="23" height="3" rx="1.5" fill="#555"></rect><rect y="8" width="23" height="3" rx="1.5" fill="#555"></rect></svg>
-			<rect y="16" width="23" height="3" rx="1.5" fill="#555"></rect>
-			<rect width="23" height="3" rx="1.5" fill="#555"></rect>
-			<rect y="8" width="23" height="3" rx="1.5" fill="#555"></rect>
-		 */}
