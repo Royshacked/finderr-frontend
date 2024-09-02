3ba7775f757bcadbbb83433619914531bee9efe3
Royshacked <roy.shacked@mail.huji.ac.il>
2024-09-02 19:01:12 +0300

    Style GigOrderIndex page

--- src/cmps/GigOrderList.jsx
+++ src/cmps/GigOrderList.jsx
@@ -4,7 +4,9 @@ import { GigOrderPreview } from "./GigOrderPreview.jsx";
 export function GigOrderList({ orders, filterBy }) {
 
     return <section className="gig-order-list">
-        {<h3>{filterBy.status ? filterBy.status.toUpperCase() : 'ALL ORDERS'}</h3>}
+        <div>
+            {<h3>{filterBy.status ? filterBy.status.toUpperCase() : 'ALL ORDERS'}</h3>}
+        </div>
         <ul>
             {orders.map(order =>
                 <li key={order._id}>
@@ -12,5 +14,6 @@ export function GigOrderList({ orders, filterBy }) {
                 </li>
             )}
         </ul>
+
     </section>
 }
--- src/cmps/GigOrderPreview.jsx
+++ src/cmps/GigOrderPreview.jsx
@@ -3,10 +3,15 @@
 
 export function GigOrderPreview({ order }) {
     return <article className="gig-order-preview">
-        {/* <img src={order.gig.imgUrl} alt="" /> */}
-        <span>{order.gig.name}</span>
-        <span>{order.createdAt}</span>
-        <span>{order.gig.price}$</span>
-        <span>{order.status}</span>
+        <div className="">
+            <img src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/241886721/original/5eb50249fc92fb86a041044def0812650db3dfdf.jpg" alt="" />
+            {/* <img src={order.gig.imgUrl} alt="" /> */}
+            <span>{order.gig.name}</span>
+        </div>
+        <div>
+            <span>{order.createdAt}</span>
+            <span>{order.gig.price}$</span>
+            <span>{order.status}</span>
+        </div>
     </article>
 }
