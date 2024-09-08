7c8cb9d46072efff87a99278a3e63eb85da9a2f8
Royshacked <roy.shacked@mail.huji.ac.il>
2024-09-08 09:05:59 +0300

    Fix no results in GigList

--- src/cmps/GigList.jsx
+++ src/cmps/GigList.jsx
@@ -1,15 +1,6 @@
 import { GigPreview } from "./GigPreview.jsx";
 
 export function GigList({ gigs }) {
-
-    // function shouldShowActionBtns(gig) {
-    //     const user = userService.getLoggedinUser()
-
-    //     if (!user) return false
-    //     if (user.isAdmin) return true
-    //     return gig.owner?._id === user._id
-    // }
-    if (!gigs.length) return <h2>loading...</h2>
     return <section>
         <ul className="gig-list">
             {gigs.map((gig) =>
