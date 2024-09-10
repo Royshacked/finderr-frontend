199d4f987d3c333e5526e8995af03435fd92464d
Royshacked <roy.shacked@mail.huji.ac.il>
2024-09-10 21:49:19 +0300

    Style GigOrderList

--- src/cmps/GigOrderPreview.jsx
+++ src/cmps/GigOrderPreview.jsx
@@ -2,8 +2,6 @@ import { useState } from "react"
 import { updateOrder } from "../store/actions/order.actions"
 
 export function GigOrderPreview({ order, filterBy }) {
-    const [orderToEdit, setOrder] = useState(order)
-
     async function onStatus(status) {
         try {
             await updateOrder({ ...order, status }, filterBy)
@@ -18,15 +16,15 @@ export function GigOrderPreview({ order, filterBy }) {
             <span>{order.gig.name}</span>
         </div>
         <div>
-            {order.status === 'pending' && <button onClick={() => onStatus('approved')}>Approve</button>}
-            {order.status === 'pending' && <button onClick={() => onStatus('rejected')}>Reject</button>}
-            {order.status === 'approved' && <button onClick={() => onStatus('completed')}>Complete</button>}
+            {order.status === 'pending' && <button onClick={() => onStatus('approved')} style={{ color: 'blue' }}>Approve</button>}
+            {order.status === 'pending' && <button onClick={() => onStatus('rejected')} style={{ color: 'red' }}>Reject</button>}
+            {order.status === 'approved' && <button onClick={() => onStatus('completed')} style={{ color: 'green' }}>Complete job</button>}
         </div>
-        <div>
-            <span>{order.createdAt}</span>
-            <span>{order.gig.price}$</span>
-            <span>{order.status}</span>
-        </div>
+        {/* <div> */}
+        <span>{order.createdAt}</span>
+        <span>{order.gig.price}$</span>
+        <span className={`status ${order.status}`}>{order.status.charAt(0).toUpperCase() + order.status.slice(1)}</span>
+        {/* </div> */}
     </article>
 }
 
