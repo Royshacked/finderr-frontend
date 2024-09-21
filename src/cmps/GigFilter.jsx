b1da40facc9da534c4ea93445998bd8684595c51
Royshacked <roy.shacked@mail.huji.ac.il>
2024-09-21 18:56:09 +0300

    Implement clr btn to search input

--- src/cmps/GigFilter.jsx
+++ src/cmps/GigFilter.jsx
@@ -9,17 +9,10 @@ import Search from '../assets/svg/homepage/search.svg?react'
 
 
 export function GigFilter() {
-    const [filterByToEdit, setFilterByToEdit] = useState(gigService.getDefaultFilter)
+    const [filterByToEdit, setFilterByToEdit] = useState(gigService.getDefaultFilter())
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-
-    function onSubmit(ev) {
-        ev.preventDefault()
-
-        dispatch({ type: SET_FILTER_BY, filterBy: { ...filterByToEdit } })
-        navigate('/gig')
-    }
 
     function onChange({ target }) {
         const { name, value } = target
@@ -27,9 +20,22 @@ export function GigFilter() {
         setFilterByToEdit(prevFilter => ({ ...prevFilter, [name]: value }))
     }
 
+    function onSubmit(ev) {
+        if (ev) ev.preventDefault()
+
+        dispatch({ type: SET_FILTER_BY, filterBy: { ...filterByToEdit } })
+        navigate('/gig')
+    }
+
+    function onClearInput() {
+        setFilterByToEdit(gigService.getDefaultFilter())
+        dispatch({ type: SET_FILTER_BY, filterBy: gigService.getDefaultFilter() })
+    }
+
     return <section className="gig-filter">
         <form onSubmit={onSubmit}>
-            <input type="text" name="title" onChange={onChange} value={filterByToEdit.title || ''} placeholder="What service are you looking for today?" />
+            <input type="text" name="title" onChange={onChange} value={filterByToEdit.title || ''} placeholder="What service are you looking for today?" autoFocus={false} />
+            {filterByToEdit.title && <button type="button" className="input-clear" onClick={onClearInput}>x</button>}
             <button><Search /></button>
         </form>
     </section>
